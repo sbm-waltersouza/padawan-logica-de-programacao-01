ef7e9b8013df557c6c7dce2234f32d66ca91096a
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-24 15:19:53 -0300

    criando logica para bolinha bater na borda e  voltar para o outro lado

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let xBolinha = 300;
 let yBolinha = 200;
 let diametro = 22;
-let velocidadeXbolinha = 6;
-let velocidadeYbolinha = 6;
+let raio = diametro / 2;
+
+let velocidadeXbolinha = 12;
+let velocidadeYbolinha = 12;
 
 function setup() {
   createCanvas(600, 400);
@@ -13,4 +15,12 @@ function draw() {
   circle(xBolinha, yBolinha, diametro);
   xBolinha += velocidadeXbolinha;
   yBolinha += velocidadeYbolinha;
+
+  if (xBolinha + raio > width || xBolinha - raio < 0) {
+    velocidadeXbolinha *= -1;
+  }
+
+  if (yBolinha + raio > height || yBolinha - raio < 0) {
+    velocidadeYbolinha *= -1;
+  }
 }
