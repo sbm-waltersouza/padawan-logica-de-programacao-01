1b6da207e0a9176e3f0788ffeb5e3b78305b8b58
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-24 14:55:49 -0300

    criando variaveis da bolinha e colocando movimento nela

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 22;
+let velocidadeXbolinha = 6;
+let velocidadeYbolinha = 6;
+
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
   background(0);
-  circle(310, 200, 20)
+  circle(xBolinha, yBolinha, diametro);
+  xBolinha += velocidadeXbolinha;
+  yBolinha += velocidadeYbolinha;
 }
