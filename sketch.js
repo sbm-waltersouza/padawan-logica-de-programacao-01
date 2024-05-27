4c6f30e68f05f9ae57cfcd32c4310d99d4623713
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-27 10:07:32 -0300

    adicionando logica para colisao da bolinha com a raquete

--- sketch.js
+++ sketch.js
@@ -1,21 +1,19 @@
-// variaveis da bolinha
+//variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
-let diametro = 22;
-let raio = diametro / 2;
+let diametro = 15;
+let raio = diametro / 2 ;
 
-// velocidade da bolinha
-let velocidadeXbolinha = 12;
-let velocidadeYbolinha = 12;
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
 
-// variaveis da raquete 
+
+//variáveis da raquete
 let xRaquete = 5;
 let yRaquete = 150;
-
-// comprimento
 let raqueteComprimento = 10;
 let raqueteAltura = 90;
-
 
 function setup() {
   createCanvas(600, 400);
@@ -24,43 +22,51 @@ function setup() {
 function draw() {
   background(0);
   mostraBolinha();
-  movimentarBolinha();
+  movimentaBolinha();
   verificaColisaoBorda();
   mostraRaquete();
   movimentaMinhaRaquete();
+  verificaColisaoRaquete();
 }
 
-function mostraBolinha() {
+function mostraBolinha(){
   circle(xBolinha, yBolinha, diametro);
-
 }
 
-function movimentarBolinha() {
-  xBolinha += velocidadeXbolinha;
-  yBolinha += velocidadeYbolinha;
-
+function movimentaBolinha(){
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
 }
 
-function verificaColisaoBorda() {
-  if (xBolinha + raio > width || xBolinha - raio < 0) {
-    velocidadeXbolinha *= -1;
+function verificaColisaoBorda(){
+  if (xBolinha + raio> width ||
+     xBolinha - raio< 0){
+    velocidadeXBolinha *= -1;
   }
-
-  if (yBolinha + raio > height || yBolinha - raio < 0) {
-    velocidadeYbolinha *= -1;
+  if (yBolinha + raio> height ||
+     yBolinha - raio < 0){
+    velocidadeYBolinha *= -1;
   }
 }
 
-function mostraRaquete() {
-  rect(xRaquete, yRaquete, raqueteComprimento, raqueteAltura);
+function mostraRaquete(){
+  rect(xRaquete, yRaquete, raqueteComprimento, 
+      raqueteAltura);
 }
 
-function movimentaMinhaRaquete() {
-  if (keyIsDown(UP_ARROW)) {
+function movimentaMinhaRaquete(){
+  if (keyIsDown(UP_ARROW)){
     yRaquete -= 10;
   }
-
-  if (keyIsDown(DOWN_ARROW)) {
+  if (keyIsDown(DOWN_ARROW)){
     yRaquete += 10;
+  }
+}
+
+function verificaColisaoRaquete(){
+  if (xBolinha - raio < xRaquete + raqueteComprimento && 
+      yBolinha - raio < yRaquete + raqueteAltura && 
+      yBolinha + raio > yRaquete){
+    velocidadeXBolinha *= -1;
   }
 }
